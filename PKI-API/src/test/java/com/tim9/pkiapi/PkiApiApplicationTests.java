eb1de03173c30136c0ac3917cc8a52e4c9de59e0
Nikola <rebracanikola@gmail.com>
2019-03-17 14:33:50 +0100

    initial commit

---
+++ PKI-API/src/test/java/com/tim9/pkiapi/PkiApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tim9.pkiapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PkiApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
