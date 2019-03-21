142b47fd4b7386e1958b9dc6a4c3c95578e7cd6d
Luka Cvetinovic <lazarevacharmonika@gmail.com>
2019-03-21 21:47:05 +0100

    SSL start

--- PKI-API/src/main/java/com/tim9/pkiapi/config/WebConfig.java
+++ PKI-API/src/main/java/com/tim9/pkiapi/config/WebConfig.java
@@ -3,6 +3,7 @@ package com.tim9.pkiapi.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -12,5 +13,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**");
+    }
+    
+    @Override
+    public void addResourceHandlers(final ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/certificates/**").addResourceLocations("classpath:/META-INF/certificates/");
     }
 }
