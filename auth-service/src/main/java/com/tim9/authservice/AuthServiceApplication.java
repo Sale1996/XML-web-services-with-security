cec7d9086426278ce3c86a74883d406ed4cea83a
Luka Cvetinovic <lazarevacharmonika@gmail.com>
2019-05-04 00:40:49 +0200

    Demonstration of loadBalancer

--- auth-service/src/main/java/com/tim9/authservice/AuthServiceApplication.java
+++ auth-service/src/main/java/com/tim9/authservice/AuthServiceApplication.java
@@ -12,6 +12,7 @@ import org.apache.http.impl.client.HttpClients;
 import org.apache.http.ssl.SSLContexts;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
