b9d74b044583c9a6e738eb8ffa788abd3106df1c
Marko <marko.markovic561@gmail.com>
2019-05-14 21:32:58 +0200

    Main frontend  - home  - accommodations

--- _frontend/ngMainApp/src/app/app-routing.module.ts
+++ _frontend/ngMainApp/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
+import { AccommodationListComponent } from './components/accommodations/accommodation-list/accommodation-list.component';
+import { HomeComponent } from './components/home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'accommodations', component: AccommodationListComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- _frontend/ngMainApp/src/app/app.module.ts
+++ _frontend/ngMainApp/src/app/app.module.ts
@@ -1,17 +1,28 @@
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './components/navigation/navigation.component';
+import { HomeComponent } from './components/home/home.component';
+import { AccommodationsComponent } from './components/accommodations/accommodations.component';
+import { AccommodationListComponent } from './components/accommodations/accommodation-list/accommodation-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationComponent
+    NavigationComponent,
+    HomeComponent,
+    AccommodationsComponent,
+    AccommodationListComponent
   ],
   imports: [
     BrowserModule,
+    NgbModule,
+    FormsModule,
+    ReactiveFormsModule,
     AppRoutingModule
   ],
   providers: [],
