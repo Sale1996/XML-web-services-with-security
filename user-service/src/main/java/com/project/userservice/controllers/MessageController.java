13e5e9c3a723c77f400f6c2e48c705c4fdd06b80
Marko <marko.markovic561@gmail.com>
2019-05-25 22:13:09 +0200

    CrossOrigin Message Controller

--- user-service/src/main/java/com/project/userservice/controllers/MessageController.java
+++ user-service/src/main/java/com/project/userservice/controllers/MessageController.java
@@ -5,6 +5,7 @@ package com.project.userservice.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,6 +25,7 @@ import io.swagger.annotations.ApiResponses;
 @RestController
 @RequestMapping("/message")
 @Api(value="message")
+@CrossOrigin("http://localhost:4200")
 public class MessageController {
 	
 	@Autowired
