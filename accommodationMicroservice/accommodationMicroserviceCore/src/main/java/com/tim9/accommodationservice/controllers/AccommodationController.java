f2b763bc40ba9bfbd28d6623a754772ca0327b05
Sale1996 <ftnmejl96@gmail.com>
2019-05-28 16:14:55 +0200

    Dodat cross origin na accommodation za Marka

--- accommodationMicroservice/accommodationMicroserviceCore/src/main/java/com/tim9/accommodationservice/controllers/AccommodationController.java
+++ accommodationMicroservice/accommodationMicroserviceCore/src/main/java/com/tim9/accommodationservice/controllers/AccommodationController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -27,6 +28,7 @@ import io.swagger.annotations.ApiResponses;
 @RestController
 @RequestMapping("/accommodations")
 @Api(value="accommodations")
+@CrossOrigin("http://localhost:4200")
 public class AccommodationController {
 
 	//@Autowired
