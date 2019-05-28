de61e3ae7a90d2677621a0ed403998e9851b5094
Sale1996 <ftnmejl96@gmail.com>
2019-05-28 16:09:44 +0200

    sitne izmene u frontu

--- _frontend/ngAgentApp/src/app/app-routing.module.ts
+++ _frontend/ngAgentApp/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AccommodationComponent } from './components/accommodation/accommodation.component';
+import { UserReviewComponent } from './components/user-review/user-review.component';
 
 const routes: Routes = [
 
-  { path: 'accommodation', component: AccommodationComponent }
+  { path: 'accommodation', component: AccommodationComponent },
+  { path: 'user-reviews', component: UserReviewComponent }
 
 ];
 
--- _frontend/ngAgentApp/src/app/app.module.ts
+++ _frontend/ngAgentApp/src/app/app.module.ts
@@ -8,7 +8,7 @@ import { AccommodationComponent } from './components/accommodation/accommodation
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
-import { UserReviewComponent } from './components/accommodation/user-review/user-review.component';
+import { UserReviewComponent } from './components/user-review/user-review.component';
 
 
 
