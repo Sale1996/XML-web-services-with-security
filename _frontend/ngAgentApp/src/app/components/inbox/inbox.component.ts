e653791dd00a3d07c7de454403cbc70a50b06229
Sale1996 <ftnmejl96@gmail.com>
2019-06-03 22:42:33 +0200

    uradjeno izlistivanje inboxa agenta (treba dorada oko css-a za neprocitane poruke)

--- _frontend/ngAgentApp/src/app/components/inbox/inbox.component.ts
+++ _frontend/ngAgentApp/src/app/components/inbox/inbox.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { MessageModalComponent } from './message-modal/message-modal.component';
+import { Message } from 'src/app/model/message.model';
+import { Observable } from 'rxjs';
+import { MessageService } from 'src/app/services/message.service';
 
 @Component({
   selector: 'app-inbox',
@@ -14,13 +17,18 @@ export class InboxComponent implements OnInit {
   collectionSize = 200;
   pageSize: number;
   pageSizes: number[] = [25, 50, 100];
+  messages$: Observable<Message[]>;
 
-  constructor(private modalService: NgbModal) { }
+  constructor(private modalService: NgbModal, private messageService: MessageService) { }
 
   ngOnInit() {
     this.pageSize = this.pageSizes[0];
+    this.getMessages();
   }
 
+  getMessages() {
+    this.messages$ = this.messageService.getMessages();
+  }
 
   openMessageModal(id: number) {
     const agentModalRef = this.modalService.open(MessageModalComponent,
--- _frontend/ngAgentApp/src/app/services/message.service.ts
+++ _frontend/ngAgentApp/src/app/services/message.service.ts
@@ -2,6 +2,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { Message } from '../model/message.model';
 
 
 const httpOptions = {
@@ -17,5 +18,7 @@ export class MessageService {
 
     constructor(private http: HttpClient) { }
 
-
+    getMessages(): Observable<Message[]> {
+        return this.http.get<Message[]>(environment.apiUrlMessage);
+    }
 }
