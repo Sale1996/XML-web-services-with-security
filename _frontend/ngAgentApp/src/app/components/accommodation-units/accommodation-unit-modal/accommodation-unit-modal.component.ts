9925e14744834b3511f059d4ac6bc11fa1c0945d
Sale1996 <ftnmejl96@gmail.com>
2019-06-06 16:07:38 +0200

    Ispravka oko otvaranja modula

--- _frontend/ngAgentApp/src/app/components/accommodation-units/accommodation-unit-modal/accommodation-unit-modal.component.ts
+++ _frontend/ngAgentApp/src/app/components/accommodation-units/accommodation-unit-modal/accommodation-unit-modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { UnitPricesModalComponent } from '../unit-prices-modal/unit-prices-modal.component';
@@ -10,7 +10,7 @@ import { UnitPricesModalComponent } from '../unit-prices-modal/unit-prices-modal
 })
 export class AccommodationUnitModalComponent implements OnInit {
 
-
+  @Input() isCreating: boolean;
   @Output() unit: EventEmitter<any> = new EventEmitter();
   unitForm: FormGroup;
 
@@ -30,9 +30,8 @@ export class AccommodationUnitModalComponent implements OnInit {
   onSubmit() {
     if (this.unitForm.valid) {
       this.unit.emit(this.unitForm.value); //as accommodationUnit
+
       this.activeModal.close();
-
-
     }
   }
 }
--- _frontend/ngAgentApp/src/environments/environment.ts
+++ _frontend/ngAgentApp/src/environments/environment.ts
@@ -4,18 +4,18 @@
 
 export const environment = {
   production: false,
-  apiUrlAccommodation: "http://localhost:8080/accommodations",
-  apiUrlAccommodationUnit: "http://localhost:8080/accommodationUnits",
-  apiUrlType: "http://localhost:8080/types",
-  apiUrlAgent: "http://localhost:8080/agent",
-  apiUrlCategory: "http://localhost:8080/categories",
-  apiUrlCity: "http://localhost:8080/cities",
-  apiUrlComment: "http://localhost:8080/comments",
-  apiUrlExtraField: "http://localhost:8080/extraFields",
-  apiUrlMessage: "http://localhost:8080/message",
+  apiUrlAccommodation: "http://localhost:8095/accommodations",
+  apiUrlAccommodationUnit: "http://localhost:8095/accommodationUnits",
+  apiUrlType: "http://localhost:8095/types",
+  apiUrlAgent: "http://localhost:8095/agent",
+  apiUrlCategory: "http://localhost:8095/categories",
+  apiUrlCity: "http://localhost:8095/cities",
+  apiUrlComment: "http://localhost:8095/comments",
+  apiUrlExtraField: "http://localhost:8095/extraFields",
+  apiUrlMessage: "http://localhost:8095/message",
   // apiUrlPicture: "https://localhost:8080/accommodations",
-  apiUrlPrice: "http://localhost:8080/price",
-  apiUrlReservation: "http://localhost:8080/reservations"
+  apiUrlPrice: "http://localhost:8095/price",
+  apiUrlReservation: "http://localhost:8095/reservations"
 };
 
 /*
